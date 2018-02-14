4e8017fc9c56da00fefdfc0bb2e0b56a823ef383
Faizy711 <faizan.s711@gmail.com>
2018-02-14 12:13:53 -0600

    class update

--- server.js
+++ server.js
@@ -19,47 +19,51 @@ app.use(bodyParser.json());
 
 var reservationList = [{
     name: "human being",
-    phoneNumber: "111-222-3333",
-    email: "human@hi.com",
-    id: "12345A"
+    email: "111-222-3333",
+    phone: "human@hi.com",
+    uniqueId: "12345A"
 }];
 var waitList = [{
-    name: "waiting human being",
-    phoneNumber: "111-222-3333",
-    email: "waitinghuman@hi.com",
-    id: "12345W"
+    name: "wait list human being",
+    email: "111-222-3333",
+    phone: "human@hi.com",
+    uniqueId: "462849"
 }];
 
 // Basic route that sends the user first to the AJAX Page
+//HTML Routes
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/resList", function (req, res) {
-    res.json(reservationList);
-});
-
-app.get("/wait", function (req, res) {
-    res.json(waitList);
-});
-
-
-app.get("/reservation", function (req, res) {
+app.get("/reservations", function (req, res) {
     res.sendFile(path.join(__dirname, "reservations.html"));
 })
 
+// API Routes
+app.get("/api/resList", function (req, res) {
+    var allReservation = {};
+    allReservation.reservationList = reservationList;
+    allReservation.waitList = waitList;
+    
+    res.json(allReservation);
+});
+
 app.post("/api/new", function (req, res) {
     var newListObj = req.body;
+    var reserved = true;
 
-    if (reservationList.length > 5) {
+    if (reservationList.length > 4) {
         waitList.push(newListObj);
-    } else {
+        res.json(!reserved);
+    } 
+    else 
+    {
         reservationList.push(newListObj);
+        res.json(reserved);
     }
-
-    res.json(newListObj);
-
 });
+
 
 
 // Starts the server to begin listening
